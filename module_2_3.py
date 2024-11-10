90a4255c5377530bf4c0431cec5ef2ebc96c5338
Alexander-222-Mor <amorgunov948@gmail.com>
2024-11-10 19:52:59 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,12 @@
+mu_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i < len(mu_list):
+    if mu_list[i] < 0:
+        break
+    elif mu_list[i] > 0:
+        print(mu_list[i])
+    i += 1
+
+
+
+
